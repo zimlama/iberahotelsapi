4247ce3454fbb4ffb78602a3a98a479913e36316
Mek <99274729+mgs1987@users.noreply.github.com>
2023-02-01 17:32:53 -0300

    rutas get añadida,cambio de nombre de base de datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const  hoteles = require("../../db_temp/hotel")
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/", (req,res,next)=>{
+    return res.status(200).json(hoteles)
+})
 
 module.exports = router;
