e0f0c63996dbf54f84ab6fed0179b6ff43cd4af6
adrianavinicki <adriana.vinicki@gmail.com>
2023-02-04 15:12:17 -0300

    2023/02/04 15 cambio en modelos timestamps

--- src/models/Amenities.js
+++ src/models/Amenities.js
@@ -1,24 +1,31 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('amenities', {
-    idAmenities: {
+  sequelize.define(
+    "amenities",
+    {
+      idAmenities: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
       },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
-    image: {
-        type : DataTypes.STRING,
-        allowNull: false
-    },
-    status: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    }  });
+    {
+      timestamps: false,
+    }
+  );
 };
--- src/models/Hotel.js
+++ src/models/Hotel.js
@@ -1,52 +1,49 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('hotel', {
-  //ID : Que lo defina el sistema
-    idHotels:{
-      type: DataTypes.INTEGER,
-       allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
+  sequelize.define(
+    "hotel",
+    {
+      //ID : Que lo defina el sistema
+      idHotels: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type : DataTypes.STRING,
-      allowNull: false
-    },
-    city: {
-      type: DataTypes.STRING,
-      allowNull : false
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    image: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false
-    },
-    stars: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false
-    },
-    createdInDb: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true,
+    {
+      timestamps: false,
     }
-  });
-}; 
-
-
-
-
+  );
+};
