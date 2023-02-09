d6dc975806f126cd74d0b97dca061dac55631808
zimlama <zimlama@gmail.com>
2023-02-08 19:56:00 -0500

    Se realiza cambio para apuntar al railway

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/hotelsIberia`, {
+//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/hotelsIberia`, {
 const sequelize = new Sequelize(`postgresql://postgres:2oNnBI3ZZ2BjWiAMBuhc@containers-us-west-182.railway.app:7595/railway`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
