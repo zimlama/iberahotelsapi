77dee0fef473ccc247609db2a37c485a40581e1a
FacuEtcheverryL <105181693+FacuEtcheverryL@users.noreply.github.com>
2023-02-18 18:10:05 -0300

    hotels create
    hotels create

--- src/db.js
+++ src/db.js
@@ -28,7 +28,7 @@ fs.readdirSync(path.join(__dirname, "/models"))
     modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+// Injectamos la conexion (sequelize) a todos los modeloss
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
