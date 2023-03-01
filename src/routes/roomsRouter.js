c8e990b1656348d184b2ecb1513f1a42aa4556b3
Molli1992 <99288359+Molli1992@users.noreply.github.com>
2023-03-01 10:51:20 -0300

    Update roomsRouter.js

--- src/routes/roomsRouter.js
+++ src/routes/roomsRouter.js
@@ -1,5 +1,6 @@
-const {Router} = require('express');
-const {getAllRooms, postNewRoom, postCreateRoomInventory, postReserveRoomInventory} = require('../controllers/roomsControllers.js');
+const { Router } = require('express');
+const { getAllRooms, postNewRoom, postCreateRoomInventory, postReserveRoomInventory, deleteRoom, DisableRoom, ModifyRoom } = require('../controllers/roomsControllers.js');
+
 
 
 const router = Router();
@@ -9,6 +10,9 @@ router.get('/', getAllRooms);
 router.post('/create', postNewRoom)
 router.post('/create/inventory', postCreateRoomInventory)
 router.post('/reserve', postReserveRoomInventory)
+router.delete("/delete/:idRooms", deleteRoom);
+router.put("/disable/:idRooms", DisableRoom)
+router.put("/modify/:idRooms", ModifyRoom)
 
 
 module.exports = router;
