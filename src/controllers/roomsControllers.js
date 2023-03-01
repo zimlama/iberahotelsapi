d23486298ffbc1ef71be613b9999e2f2a1f55af6
Molli1992 <99288359+Molli1992@users.noreply.github.com>
2023-03-01 10:52:08 -0300

    Update roomsControllers.js

--- src/controllers/roomsControllers.js
+++ src/controllers/roomsControllers.js
@@ -1,4 +1,4 @@
-const { Room, Inventory,  Typeofroom} = require("../db");
+const { Room, Inventory, Typeofroom } = require("../db");
 const { Op } = require("sequelize");
 const { containLettersCheck, containNumbersCheck, onlyNumbersCheck } = require('../helpfuls/regex');
 
@@ -8,7 +8,7 @@ const getAllRooms = async (req, res) => {
     const allRooms = await Room.findAll({});
     return res.status(200).send(allRooms);
   } catch (err) {
-    res.status(400).json({ error: err});
+    res.status(400).json({ error: err });
   }
 };
 
@@ -42,41 +42,131 @@ const postNewRoom = async (req, res) => {
 };
 
 //! POST create room inventory --------------
-async function postCreateRoomInventory(req, res){
-  try{
-      let { RoomNumber, idHotels, idTypeofrooms }  = req.body;
-      console.log("esto es RoomNumber.length: ", RoomNumber.toString().length)
-      if ( RoomNumber.toString().length !== 4){
-          return res.status(412).send({ message: "information required" });
-      }
-      City = idHotels.slice(0,5);
-      Hotel = parseInt(idHotels.slice(5));
-      let roomInventory = { City, Hotel, RoomNumber, idHotels, idTypeofrooms };
-      await Inventory.findOrCreate({
-        where: roomInventory,
-      });
-      return res.status(201).send({ message: "Room was created" });
-  } catch(err){
-      res.status(500).json({ error: err});
+async function postCreateRoomInventory(req, res) {
+  try {
+    let { idRoomInventory, id } = req.body;
+    if (!containLettersCheck(idRoomInventory) && !containNumbersCheck(idRoomInventory) || idRoomInventory.length !== 11) {
+      return res.status(412).send({ message: "information required" });
+    }
+    let roomInventory = { idRoomInventory, id };
+    await Inventory.findOrCreate({
+      where: roomInventory,
+    });
+    return res.status(201).send({ message: "Room was created" });
+  } catch (err) {
+    res.status(500).json({ error: err });
   };
 }
 //!--------------
 
 //! POST reserve room inventory --------------
-async function postReserveRoomInventory(req, res){
-  try{
+async function postReserveRoomInventory(req, res) {
+  try {
     let { idUser, id, checkin, checkout } = req.body;
     console.log("esto es idUser: ", idUser);
     console.log("esto es idUser: ", id);
     console.log("esto es checkin: ", checkin);
     console.log("esto es checkout: ", checkout);
     return res.status(201).send({ message: idUser, id });
-  } catch(err){
-    res.status(500).json({ error: error});
+  } catch (err) {
+    res.status(500).json({ error: error });
   }
 }
 
-//!--------------
+//!-------------- Delete Room ----------------------
+
+const deleteRoom = async (req, res) => {
+
+  try {
+
+    let { idRooms } = req.params;
+
+    Room.destroy({
+      where: {
+        idRooms: idRooms
+      }
+    })
+
+    res.status(200).json({ message: "Room deleted" });
+
+
+  } catch (error) {
+
+    console.log(error);
+
+  };
+
+};
+
+//------------------Disable Rooom ------------------------
+
+async function DisableRoom(req, res) {
+
+  try {
+
+    let { idRooms } = req.params;
+
+    const room = await Room.findOne({
+      where: {
+        idRooms: idRooms
+      }
+    });
+
+    if (room.status === true) {
+      room.update({ status: false });
+    } else if (room.status === false) {
+      room.update({ status: true });
+    }
+
+    res.send(room);
+
+  } catch (error) {
+
+    console.log(error);
+
+  };
+
+};
+
+//------------------Modify Rooom ------------------------
+
+async function ModifyRoom(req, res) {
+
+  try {
+
+    let { idRooms } = req.params;
+    let { name, bed_quantity, price, description } = req.body;
+
+    const room = await Room.findOne({
+      where: {
+        idRooms: idRooms
+      }
+    });
+
+    if (room) {
+
+      room.update({
+        name: name,
+        bed_quantity: bed_quantity,
+        price: price,
+        description: description
+      });
+
+      res.send(room);
+
+    } else {
+
+      res.send("Room not found");
+
+    }
+
+  } catch (error) {
+
+    console.log(error);
+
+  };
+
+};
 
 
 //!!!!
@@ -84,5 +174,8 @@ module.exports = {
   getAllRooms,
   postNewRoom,
   postCreateRoomInventory,
-  postReserveRoomInventory
+  postReserveRoomInventory,
+  deleteRoom,
+  DisableRoom,
+  ModifyRoom
 };
