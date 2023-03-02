ba64cb88b767ec4f039df34e8344ac587833a229
Mek <99274729+mgs1987@users.noreply.github.com>
2023-03-02 20:17:51 -0300

    cambios facu

---
+++ src/routes/reviewsRouter.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const {
+  postReview,
+  getReviewsByHotelId,
+  deleteReview,
+} = require("../controllers/reviewsControllers");
+
+const router = Router();
+
+router.post("/create", postReview);
+router.get("/:hotelId", getReviewsByHotelId);
+router.delete("/:id", deleteReview);
+
+module.exports = router;
