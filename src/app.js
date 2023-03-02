b95bbab2d727bd023d04d25414c54482f3c53f35
Leonardo Mejia <zimlama@gmail.com>
2023-03-02 11:48:01 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const server = express();
 // segunda vuelta
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
