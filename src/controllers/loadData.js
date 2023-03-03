e9f8a159900e4de3b5d2e64ff8349314d756b19e
zimlama <zimlama@gmail.com>
2023-03-03 11:20:33 -0500

    arreglos db crash emptyHotel

--- src/controllers/loadData.js
+++ src/controllers/loadData.js
@@ -19,8 +19,11 @@ const loadCat_room_inventory03 = require('../data/cat_room_inventory03.json');
 
 async function loadAllModelsInDB(){
   try{
-    await Hotel.bulkCreate(loadhotels);
+    const emptyHotel = await Hotel.findAll({});
+    if(!emptyHotel){
+      await Hotel.bulkCreate(loadhotels);
     console.log('Hotels loaded ok to DB')
+    }
     await Room.bulkCreate(loadRooms);
     console.log('Rooms loaded ok to DB')
     await Amenities.bulkCreate(loadAmenities);
