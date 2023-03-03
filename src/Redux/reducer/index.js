bd22ea234bd05571b9034514623c7ee928f35ec6
zimlama <zimlama@gmail.com>
2023-03-03 08:27:10 -0500

    update meky caritas

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -101,6 +101,11 @@ export default function rootReducer(state = initialState, action) {
       return {
         ...state,
       };
+    case GET_CLOUDINARY_IMG:
+      return {
+        ...state,
+        productImg: action.payload,
+      };
 
     default:
       return state;
